2692dda900b59b8221d5009ff7447a096d5ea092
Felipe Sales <soufeliposales@gmail.com>
2023-05-31 22:06:13 -0300

    feat: init v1-get-professionals requests

--- cloud/main.js
+++ cloud/main.js
@@ -1,6 +1,47 @@
+const Professional = Parse.Object.extend('Professional');
+const Specialty = Parse.Object.extend('Specialty');
 
-// Use Parse.Cloud.define to define as many cloud functions as you want.
-// For example:
-Parse.Cloud.define("hello", (request) => {
-	return "Hello world!";
+Parse.Cloud.define('v1-get-professionals', async (req) => {
+    const query = new Parse.Query(Professional);
+	query.include('specialties', 'insurences', 'services');
+
+	if (req.params.specialtyId) {
+		const specialty = new Specialty();
+		specialty.id = req.params.specialtyId;
+		query.equalTo('specialties', specialty);
+	}
+
+	if (req.params.lat && req.params.lng) {
+		const point = new Parse.GeoPoint({latitude: req.params.lat, longitude: req.params.lng});
+		query.withinKilometers('location', point, req.params.maxDistance || 100);
+	}
+
+	if (req.params.limit && req.params.skip) {
+		query.limit(req.params.limit);
+		query.skip(req.params.skip);
+	}
+
+	const results = await query.find({useMasterKey: true});
+	// return results;
+	return results.map((r) => formatProfessional(r.toJSON()));
+}, {
+	fields: {
+		specialtyId: {}
+	},
 });
+
+function formatProfessional(p) {
+	return {
+		id: p.objectId,
+		name: p.name,
+		specialties: p.specialties.map((s) => formatSpecialty(s)),
+		crm: p.crm,
+	}
+}
+
+function formatSpecialty(s) {
+	return {
+		id: s.objectId,
+		name: s.name,
+	}
+}
