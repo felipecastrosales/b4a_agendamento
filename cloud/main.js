db4e55c7a3008da296f84541114ccaf1d606e443
Felipe Sales <soufeliposales@gmail.com>
2023-06-05 21:59:17 -0300

    feat: add more functions on Parse

--- cloud/main.js
+++ cloud/main.js
@@ -1,6 +1,54 @@
 const Professional = Parse.Object.extend('Professional');
 const Specialty = Parse.Object.extend('Specialty');
 
+Parse.Cloud.define('v1-sign-in', async (req) => {
+	const user = Parse.User.signIn(req.params.email.toLowerCase(), req.params.password);
+	return formatUser(user.toJSON());
+}, {
+	fields: {
+		email: {
+			required: true,
+		},
+		password: {
+			required: true,
+		},
+	}
+});
+
+Parse.Cloud.define('v1-get-user', async (req) => {
+	return formatUser(req.user.toJSON());
+});
+
+Parse.Cloud.define('v1-sign-up', async (req) => {
+	const user = new Parse.User();
+	user.set('email', req.params.email.toLowerCase());
+	user.set('username', req.params.email.toLowerCase());
+	user.set('fullname', req.params.fullName);
+	user.set('phone', req.params.phone);
+	user.set('document', req.params.document);
+	user.set('password', req.params.password);
+	await user.signUp(null, {useMasterKey: true});
+	return formatUser(user.toJSON());
+}, {
+	fields: {
+		email: {
+			required: true,
+		},
+		password: {
+			required: true,
+		},
+		fullname: {
+			required: true,
+		},
+		document: {
+			required: true,
+		},
+		phone: {
+			required: true,
+		},
+	}
+});
+
 Parse.Cloud.define('v1-get-professionals', async (req) => {
     const query = new Parse.Query(Professional);
 	query.include('specialties', 'insurences', 'services');
@@ -43,5 +91,14 @@ function formatSpecialty(s) {
 	return {
 		id: s.objectId,
 		name: s.name,
+	}
+}
+
+function formatUser(u) {
+	return {
+		id: u.objectId,
+		fullname: u.fullname,
+		document: u.document,
+		phone: u.phone,
 	}
 }
